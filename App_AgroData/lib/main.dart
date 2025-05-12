3c917f10397f08e3b1da149e1e35a54c2dce2c5a
Jonathan Catalan <jluiscatalan1@gmail.com>
2025-05-11 22:53:48 -0400

    Limpieza de comentarios

--- App_AgroData/lib/main.dart
+++ App_AgroData/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'Pages/home.dart';
 import 'Pages/new_Harvest.dart';
 import 'Pages/profile.dart';
-import 'Pages/splash.dart';
+import 'Pages/splash.dart ';
 
 void main() {
   runApp(const MyApp());
@@ -17,21 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
