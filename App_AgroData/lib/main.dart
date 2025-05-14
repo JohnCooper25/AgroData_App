125bdd59b82bb2b9a63d55ea87627233c3526db2
Jonathan Catalan <jluiscatalan1@gmail.com>
2025-05-14 14:01:03 -0400

    Cambio de textos

--- App_AgroData/lib/main.dart
+++ App_AgroData/lib/main.dart
@@ -15,12 +15,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Agro_Data',
       theme: ThemeData(
        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'AgroData'),
     );
   }
 }
